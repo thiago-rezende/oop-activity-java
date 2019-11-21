53b0788eedba7c0db404ec294678ee3d12ccedb6
Thiago Rezende <bitrateti@gmail.com>
2019-11-20 23:43:12 -0300

    added the EnrolledSubject class

---
+++ src/main/java/app/EnrolledSubject.java
@@ -0,0 +1,63 @@
+package app;
+
+import java.time.LocalDate;
+
+public class EnrolledSubject {
+
+    private Subject m_subject;
+    private float m_grade;
+    private LocalDate m_dateOfRegistration;
+
+    public EnrolledSubject() {
+        m_subject = new Subject();
+        m_grade = 0;
+        m_dateOfRegistration = LocalDate.now();
+    }
+
+    public EnrolledSubject(Subject subject, int grade) {
+        m_subject = subject;
+        m_grade = grade;
+    }
+
+    public EnrolledSubject(Subject subject) {
+        m_subject = subject;
+        m_grade = 0;
+    }
+
+    public Subject getSubject() {
+        return m_subject;
+    }
+
+    public void setSubject(Subject subject) {
+        m_subject = subject;
+    }
+
+    public float getGrade() {
+        return m_grade;
+    }
+
+    public void setGrade(int grade) {
+        m_grade = grade;
+    }
+
+    public LocalDate getDateOfRegistration() {
+        return m_dateOfRegistration;
+    }
+
+    public void setDateOfRegistration(LocalDate dateOfRegistration) {
+        m_dateOfRegistration = dateOfRegistration;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        EnrolledSubject other = (EnrolledSubject) obj;
+        return this.getSubject().equals(other.getSubject());
+    }
+
+    @Override
+    public String toString() {
+        return "EnrolledSubject {Subject: " + this.getSubject() + ", Grade: " + this.getGrade()
+                + ", Date of registration: " + this.getDateOfRegistration() + "}";
+    }
+
+}
